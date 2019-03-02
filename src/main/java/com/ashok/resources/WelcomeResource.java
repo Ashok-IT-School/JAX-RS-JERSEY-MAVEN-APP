309266557b4c1b0e5e0e4bf386b7f2d93c4c6664
Ashok IT <30829130+Ashok-IT-School@users.noreply.github.com>
2019-03-02 20:43:46 +0530

    Add files via upload

---
+++ src/main/java/com/ashok/resources/WelcomeResource.java
@@ -0,0 +1,30 @@
+package com.ashok.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * This class is exposed as Rest Resource
+ * 
+ * @author Ashok
+ *
+ */
+@Path("/welcome")
+public class WelcomeResource {
+
+	public WelcomeResource() {
+		System.out.println("WelcomeResource:::Constructor");
+	}
+
+	/**
+	 * This method is used to handle GET request
+	 * http://localhost:<port-num>/JAX-RS-JERSEY-APP/rest/welcome
+	 * 
+	 * @return String
+	 */
+	@GET
+	public String wish() {
+		return "<h1>Welcome to Restful Services...!!</h1>";
+	}
+
+}
